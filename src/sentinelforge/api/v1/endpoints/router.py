5b211296acdbb9bdbf2ff75a233188ae6f39de6d
Henriquesilva0328 <henry.silva4246@gmail.com>
2026-04-18 17:03:55 -0300

    Move router.py

--- src/sentinelforge/api/v1/endpoints/router.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from sentinelforge.api.v1.endpoints.health import router as health_router
-from sentinelforge.api.v1.endpoints.ingest import router as ingest_router
-
-api_router = APIRouter()
-
-api_router.include_router(health_router)
-api_router.include_router(ingest_router)
